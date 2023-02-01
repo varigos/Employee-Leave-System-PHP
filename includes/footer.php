1da7794519028b9ce3fe736eb53bbc0384d20c46
Ahmed.H <124135152+varigos@users.noreply.github.com>
2023-02-01 12:05:24 +0000

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By Ahmed Kamal</p>
+        </div>
+</footer>
